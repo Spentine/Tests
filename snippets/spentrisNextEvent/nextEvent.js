f1e129e0ebfbe767117ea4ec24a3145197b0105c
Spentine <114318049+Spentine@users.noreply.github.com>
2024-10-30 19:28:44 -0700

    gaEventHandler

---
+++ snippets/spentrisNextEvent/nextEvent.js
@@ -0,0 +1,139 @@
+/*
+  Usage
+  
+  gravSpeed and arrSpeed
+    time / tick
+  
+  .next(): gets the next input
+  .skip(): skips the input
+  
+  Output
+  
+  {
+    "action": (gravity / arr),
+    "time": (time at which it occurred)
+  }
+*/
+
+export class gaEventHandler { // gravity/arr event handler
+  constructor(gravOffset, gravSpeed, arrOffset, arrSpeed, time) {
+    // initialize parameters
+    this.gravOffset = gravOffset;
+    this.gravSpeed = gravSpeed; // ms / drop
+    this.arrOffset = arrOffset;
+    this.arrSpeed = arrSpeed;
+    this.time = time;
+    
+    // calculate next of the events
+    this.calculateNextGrav();
+    this.calculateNextArr();
+    
+    // previous event is grav?
+    this.prev = null;
+  }
+  
+  // s (floor((time - o) / s) + 1) + o for next
+  calculateNextGrav() {
+    this.gravNext = this.gravSpeed * (Math.floor((this.time - this.gravOffset) / this.gravSpeed) + 1) + this.gravOffset;
+  }
+  calculateNextArr() {
+    this.arrNext = this.arrSpeed * (Math.floor((this.time - this.arrOffset) / this.arrSpeed) + 1) + this.arrOffset;
+  }
+  
+  // creates new event
+  
+  createEvent(type, time) {
+    this.prev = type === "gravity";
+    this.time = time;
+    return {
+      "action": type,
+      "time": time
+    };
+  }
+  
+  /*
+    next()
+    - Jumps to the next event
+    - Provides the event type and time
+    
+    if if the last action was gravity and this.arrNext = 0:
+      send arr
+    
+    update next values
+    
+    if: this.arrNext and this.gravNext are different
+      use the one that's more recent
+    
+    else if: this.arrNext and this.gravNext are the same
+      send gravity first
+  */
+  
+  next() {
+    // gravity then arr condition
+    if (this.prev && this.arrNext === this.time) {
+      // update it otherwise you fall into an infinite loop
+      this.calculateNextArr();
+      // send event
+      return this.createEvent("arr", this.time);
+    }
+    
+    // calculate the next occurrence
+    if (this.arrNext <= this.time) {
+      this.calculateNextArr();
+    }
+    if (this.gravNext <= this.time) {
+      this.calculateNextGrav();
+    }
+    // console.log(this.arrNext, this.gravNext);
+    
+    // decide which one to send
+    if (this.arrNext < this.gravNext) {
+      return this.createEvent("arr", this.arrNext);
+    } else {
+      return this.createEvent("gravity", this.gravNext);
+    }
+  }
+  
+  /*
+    skip()
+    - Jumps over all of the same event
+    - Changes the offset of gravity
+    - Provides event type and time
+    
+    if the last action was arr:
+      jump to the next gravity event
+    
+    else if this.arrNext is the same as time:
+      use this.arrNext as the next event
+      (note: it's not possible to jump to the next arr event because it's exclusive of 0)
+    
+    else (if the last action was gravity):
+      jump to the next arr event
+  */
+  
+  skip() {
+    if (this.prev === null) {
+      return; // there is no case where you skip immediately
+    }
+    
+    // jump to gravity event
+    if (!this.prev) {
+      this.calculateNextGrav();
+      return this.createEvent("gravity", this.gravNext);
+    }
+    
+    // arr and gravity on same time
+    if (this.arrNext === this.time) {
+      return this.createEvent("arr", this.arrNext);
+    }
+    
+    // jump to arr
+    this.calculateNextArr();
+    return this.createEvent("arr", this.arrNext);
+  }
+  
+  // if the block hits the ground the gravity time gets reset
+  resetGravityTime() {
+    this.gravOffset = this.time;
+  }
+}
---
+++ snippets/spentrisNextEvent/test.js
@@ -0,0 +1,183 @@
+import { assertEquals } from "jsr:@std/assert";
+import { gaEventHandler } from "./nextEvent.js";
+
+/*
+  n: next
+  s: skip
+*/
+
+const runActions = (ga, actions, expected) => {
+  for (let i=0; i<actions.length; i++) {
+    const action = actions[i];
+    let event;
+    if (action === "n") {
+      event = ga.next();
+    } else {
+      event = ga.skip();
+      ga.resetGravityTime(); // pretend the block hit the ground
+    }
+    console.log(i, action, expected[i], event.action[0] + event.time);
+    assertEquals(event.action[0], expected[i][0]);
+    assertEquals(event.time, Number(expected[i].substring(1)));
+  }
+}
+
+Deno.test("next() test 1", () => {
+  const ga = new gaEventHandler(
+    0, // gravOffset
+    2, // gravSpeed
+    0, // arrOffset
+    3, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 0123456
+     grav | - # # #
+      arr | -  #  #
+    event | n n n n
+          |    n
+  */
+  
+  runActions(ga, "nnnnn", ["g2", "a3", "g4", "g6", "a6"]);
+});
+
+Deno.test("next() test 2", () => {
+  const ga = new gaEventHandler(
+    0, // gravOffset
+    1, // gravSpeed
+    1, // arrOffset
+    2, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 0123456
+     grav | -######
+      arr |  # # # 
+    event | n n n n
+          |    n
+  */
+  
+  runActions(ga, "nnnnn", ["g1", "a1", "g2", "g3", "a3"]);
+});
+
+Deno.test("next() test 3", () => {
+  const ga = new gaEventHandler(
+    0, // gravOffset
+    1, // gravSpeed
+    1, // arrOffset
+    2, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 0123456
+     grav | -######
+      arr |  # # # 
+    event | nnnn
+          |  n
+  */
+  
+  runActions(ga, "nnnnn", ["g1", "a1", "g2", "g3", "a3"]);
+});
+
+Deno.test("next() test 4", () => {
+  const ga = new gaEventHandler(
+    1, // gravOffset
+    3, // gravSpeed
+    2, // arrOffset
+    8, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 0123456789abcdefghi
+     grav |  #  #  #  #  #  #
+      arr |   #       #       #
+    event | nn  n  n  n  n  n
+          |   n       n       
+  */
+  
+  runActions(ga, "nnnnnnnnn", ["g1", "a2", "g4", "g7", "g10", "a10", "g13", "g16", "a18"]);
+});
+
+Deno.test("skip() test 1", () => {
+  const ga = new gaEventHandler(
+    0, // gravOffset
+    2, // gravSpeed
+    0, // arrOffset
+    3, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 012345678
+     grav | - #x #x #
+      arr | -  #  #  
+    event | n s  s  
+          |    s  s
+  */
+  
+  runActions(ga, "nssss", ["g2", "a3", "g5", "a6", "g8"]);
+});
+
+Deno.test("skip() test 2", () => {
+  const ga = new gaEventHandler(
+    0, // gravOffset
+    1, // gravSpeed
+    1, // arrOffset
+    2, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 0123456
+     grav | -######
+      arr |  # # # 
+    event | nsss
+          |  s
+  */
+  
+  runActions(ga, "nssss", ["g1", "a1", "g2", "a3", "g4"]);
+});
+
+Deno.test("skip() test 3", () => {
+  const ga = new gaEventHandler(
+    2, // gravOffset
+    3, // gravSpeed
+    1, // arrOffset
+    4, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 0123456789abc
+     grav |   #  x  #x  #
+      arr |  #   #   #
+    event | n s     s
+          |  s   s   s
+  */
+  
+  runActions(ga, "nsssss", ["a1", "g2", "a5", "g8", "a9", "g12"]);
+});
+
+Deno.test("skip() test 4", () => {
+  const ga = new gaEventHandler(
+    2, // gravOffset
+    3, // gravSpeed
+    0, // arrOffset
+    8, // arrSpeed
+    0  // time
+  );
+  
+  /*
+     time | 0123456789abcdefghijklmnopqrstuvwyxz
+     grav |   #  #  #  #  # x  #  # x  #  # x  # 
+      arr | -       #       #       #       #
+    event | n s        s       s       s
+          |         s       s       s       s
+  */
+  
+  runActions(ga, "nssssssss", ["g2", "a8", "g11", "a16", "g19", "a24", "g27", "a32", "g35"]);
+});
