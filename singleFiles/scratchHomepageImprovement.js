90293d48446a27f9ec52bc95522f0b5495d0e471
Spentine <114318049+Spentine@users.noreply.github.com>
2025-06-20 20:47:50 +0200

    use template for userscript

---
+++ singleFiles/scratchHomepageImprovement.js
@@ -0,0 +1,16 @@
+// ==UserScript==
+// @name         Scratch Homepage Improvement
+// @namespace    http://tampermonkey.net/
+// @version      2025-06-20
+// @description  Removes useless stuff from Scratch's homepage.
+// @author       Spentine
+// @match        https://scratch.mit.edu/
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=mit.edu
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
